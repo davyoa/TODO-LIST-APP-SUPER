8303a739c980fbab398e516aa9ade2937f04ddf9
David Houde <davidyoa26@gmail.com>
2025-08-28 19:39:59 +0000

    So binds to the servers address

--- backend/main.py
+++ backend/main.py
@@ -56,4 +56,4 @@ def delete_todo(id):
     return jsonify({"message": "Todo deleted"})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    app.run(debug=True, host="0.0.0.0", port=8000)
